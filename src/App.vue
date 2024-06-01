6b02cc79848eb2c0d000cdd992270c23e40b2587
wlz2022 <2667461198@qq.com>
2024-06-01 23:37:00 +0800

    add hash mode

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="严景倩" />
+      <HelloWorld msg="严景倩2.0" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
