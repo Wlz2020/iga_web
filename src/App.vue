be3b2e019cb48d1c47a1db91108cf4230be6d759
wlz2022 <2667461198@qq.com>
2024-06-01 23:04:57 +0800

    test msg

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="老婆啊" />
+      <HelloWorld msg="严景倩" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
