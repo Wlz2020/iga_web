3c25d30a374fe54057236da608a2ccbbadb17bd9
wlz2022 <2667461198@qq.com>
2024-06-01 18:55:01 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  // eslint-disable-next-line no-undef
+  base: process.env.NODE_ENV === 'production' ? '/iga_web/' : '/'
+})
